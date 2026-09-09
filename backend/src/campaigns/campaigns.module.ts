30a7705d0b435195ad209b0c4c4d06087de0e8c5
Day-Cost <dayjfdireito@outlook.com>
2026-09-09 03:23:16 +0200

    Add campaigns module

---
+++ backend/src/campaigns/campaigns.module.ts
@@ -0,0 +1,1 @@
+import {Module} from '@nestjs/common';import {CampaignsController} from './campaigns.controller';import {CampaignsService} from './campaigns.service';import {PrismaService} from '../prisma.service';@Module({controllers:[CampaignsController],providers:[CampaignsService,PrismaService]})export class CampaignsModule{}
