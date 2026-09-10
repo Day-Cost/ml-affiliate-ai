00f8377aa739352d62bc23cc880f417a215e5a39
Day-Cost <dayjfdireito@outlook.com>
2026-09-10 19:29:48 +0200

    Register automation controller

--- backend/src/automation/automation.module.ts
+++ backend/src/automation/automation.module.ts
@@ -1,6 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AutomationService } from './automation.service';
+import { AutomationController } from './automation.controller';
+import { TestMediaController } from './test-media.controller';
 import { ProductsModule } from '../products/products.module';
 
-@Module({ imports: [ProductsModule], providers: [AutomationService], exports: [AutomationService] })
+@Module({
+  imports: [ProductsModule],
+  controllers: [AutomationController, TestMediaController],
+  providers: [AutomationService],
+  exports: [AutomationService],
+})
 export class AutomationModule {}
