6c77b327c3cd552f17154e4ba94836cf4a697c17
Day-Cost <dayjfdireito@outlook.com>
2026-09-10 09:54:01 +0200

    Fix campaigns AuthService dependency

--- backend/src/campaigns/campaigns.module.ts
+++ backend/src/campaigns/campaigns.module.ts
@@ -1,1 +1,12 @@
-import {Module} from '@nestjs/common';import {CampaignsController} from './campaigns.controller';import {CampaignsService} from './campaigns.service';import {PrismaService} from '../prisma.service';@Module({controllers:[CampaignsController],providers:[CampaignsService,PrismaService]})export class CampaignsModule{}
+import { Module } from '@nestjs/common';
+import { CampaignsController } from './campaigns.controller';
+import { CampaignsService } from './campaigns.service';
+import { PrismaService } from '../prisma.service';
+import { AuthModule } from '../auth.module';
+
+@Module({
+  imports: [AuthModule],
+  controllers: [CampaignsController],
+  providers: [CampaignsService, PrismaService],
+})
+export class CampaignsModule {}
