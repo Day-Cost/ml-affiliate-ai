0278c3b05edf0ac05c1d38b85eb16960353300ef
Day-Cost <dayjfdireito@outlook.com>
2026-09-10 19:32:47 +0200

    Provide crypto service to Product Hunter

--- backend/src/products/products.module.ts
+++ backend/src/products/products.module.ts
@@ -3,12 +3,13 @@ import { ProductsController } from './products.controller';
 import { ProductHunterService } from './product-hunter.service';
 import { ScoringModule } from '../scoring/scoring.module';
 import { PrismaService } from '../prisma.service';
+import { CryptoService } from '../crypto.service';
 import { AuthModule } from '../auth.module';
 
 @Module({
   imports: [ScoringModule, AuthModule],
   controllers: [ProductsController],
-  providers: [ProductHunterService, PrismaService],
+  providers: [ProductHunterService, PrismaService, CryptoService],
   exports: [ProductHunterService],
 })
 export class ProductsModule {}
