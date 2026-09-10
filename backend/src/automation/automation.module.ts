295eb1cd2638db6ce6c778660fa1771458856b7f
Day-Cost <dayjfdireito@outlook.com>
2026-09-10 19:31:20 +0200

    Add AuthModule to automation controls

--- backend/src/automation/automation.module.ts
+++ backend/src/automation/automation.module.ts
@@ -3,9 +3,10 @@ import { AutomationService } from './automation.service';
 import { AutomationController } from './automation.controller';
 import { TestMediaController } from './test-media.controller';
 import { ProductsModule } from '../products/products.module';
+import { AuthModule } from '../auth.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [ProductsModule, AuthModule],
   controllers: [AutomationController, TestMediaController],
   providers: [AutomationService],
   exports: [AutomationService],
