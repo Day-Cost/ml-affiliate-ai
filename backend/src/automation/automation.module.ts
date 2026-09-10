31078c189a3c482f408fded3593aca98b2864ec8
Day-Cost <dayjfdireito@outlook.com>
2026-09-10 19:34:16 +0200

    Register product video renderer

--- backend/src/automation/automation.module.ts
+++ backend/src/automation/automation.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { AutomationService } from './automation.service';
 import { AutomationController } from './automation.controller';
 import { TestMediaController } from './test-media.controller';
+import { ProductVideoController } from './product-video.controller';
 import { ProductsModule } from '../products/products.module';
 import { AuthModule } from '../auth.module';
 
 @Module({
   imports: [ProductsModule, AuthModule],
-  controllers: [AutomationController, TestMediaController],
+  controllers: [AutomationController, TestMediaController, ProductVideoController],
   providers: [AutomationService],
   exports: [AutomationService],
 })
